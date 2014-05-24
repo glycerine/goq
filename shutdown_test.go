5999cb82989cee866308f957959d2c4835784437
glycerine <j.e.aten@gmail.com>
2014-05-23 17:05:24 -0700

    don't 2x shutdown in shutdown_test, as it deadlocks

--- shutdown_test.go
+++ shutdown_test.go
@@ -9,31 +9,30 @@ import (
 func TestLocalNanomsgBasedShutdown(t *testing.T) {
 
 	cv.Convey("shutdown after nanomsg communication should be clean", t, func() {
-		cv.Convey("even if we use mangos", func() {
 
-			// *** universal test cfg setup
-			skipbye := false
-			cfg := NewTestConfig()
-			defer cfg.ByeTestConfig(&skipbye)
-			// *** end universal test setup
+		// *** universal test cfg setup
+		skipbye := false
+		cfg := NewTestConfig()
+		defer cfg.ByeTestConfig(&skipbye)
+		// *** end universal test setup
 
-			jobserv, err := NewJobServ(cfg)
-			if err != nil {
-				panic(err)
-			}
-			remote := false
-			jobservPid := 0
-			defer CleanupServer(cfg, jobservPid, jobserv, remote, &skipbye)
-			defer CleanupOutdir(cfg)
+		_, err := NewJobServ(cfg)
+		if err != nil {
+			panic(err)
+		}
+		//remote := false
+		//jobservPid := 0
+		// we do this with SubmitShutdownJob() below: defer CleanupServer(cfg, jobservPid, jobserv, remote, &skipbye)
+		defer CleanupOutdir(cfg)
 
-			sub, err := NewSubmitter(GenAddress(), cfg, false)
-			if err != nil {
-				panic(err)
-			}
-			sub.SubmitShutdownJob()
+		sub, err := NewSubmitter(GenAddress(), cfg, false)
+		if err != nil {
+			panic(err)
+		}
+		sub.SubmitShutdownJob()
 
-			//cv.So(jobout.Out[1], cv.ShouldEqual, "I'm done with my work")
+		//cv.So(jobout.Out[1], cv.ShouldEqual, "I'm done with my work")
 
-		})
+		// we should not hang on shutdown...
 	})
 }
