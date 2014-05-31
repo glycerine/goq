82d3e0f071a9d6c81ccc9b0ffb11d903c7f8b19f
glycerine <j.e.aten@gmail.com>
2014-05-31 05:46:35 -0700

    factor serialization into ser.go. Server state saved at each heartbeat. atg.

--- goq.go
+++ goq.go
@@ -8,7 +8,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"math/rand"
 	"net"
@@ -18,7 +17,6 @@ import (
 	"time"
 
 	schema "github.com/glycerine/goq/schema"
-	capn "github.com/glycerine/go-capnproto"
 	nn "github.com/op/go-nanomsg"
 	//nn "bitbucket.org/gdamore/mangos/compat"
 )
@@ -1349,91 +1347,6 @@ func sendZjobWithoutStamping(nnzbus *nn.Socket, j *Job, cfg *Config) ([]byte, er
 
 }
 
-func StringSliceToCapnp(a []string, seg *capn.Segment) *capn.TextList {
-	if a == nil {
-		panic("string slice can't be nil")
-	}
-	if len(a) > 0 {
-		tl := seg.NewTextList(len(a))
-		for i := range a {
-			tl.Set(i, a[i])
-		}
-		return &tl
-	}
-	return nil
-}
-
-// once you've already allocated a segment
-func JobToCapnpSegment(j *Job, seg *capn.Segment) schema.Zjob {
-
-	zjob := schema.NewZjob(seg)
-
-	zjob.SetId(j.Id)
-	zjob.SetAboutjid(j.Aboutjid)
-	zjob.SetMsg(j.Msg)
-
-	zjob.SetCmd(j.Cmd)
-
-	if tl := StringSliceToCapnp(j.Out, seg); tl != nil {
-		zjob.SetOut(*tl)
-	}
-
-	if tl := StringSliceToCapnp(j.Args, seg); tl != nil {
-		zjob.SetArgs(*tl)
-	}
-
-	if tl := StringSliceToCapnp(j.Env, seg); tl != nil {
-		zjob.SetEnv(*tl)
-	}
-
-	zjob.SetHost(j.Host)
-
-	zjob.SetStm(int64(j.Stm))
-	zjob.SetEtm(int64(j.Etm))
-	zjob.SetElapsec(j.Elapsec)
-
-	zjob.SetStatus(j.Status)
-	zjob.SetSubtime(int64(j.Subtime))
-	zjob.SetPid(j.Pid)
-
-	zjob.SetDir(j.Dir)
-
-	zjob.SetSubmitaddr(j.Submitaddr)
-	zjob.SetServeraddr(j.Serveraddr)
-	zjob.SetWorkeraddr(j.Workeraddr)
-
-	if tl := StringSliceToCapnp(j.Finishaddr, seg); tl != nil {
-		zjob.SetFinishaddr(*tl)
-	}
-
-	zjob.SetSignature(j.Signature)
-	zjob.SetIslocal(j.IsLocal)
-	zjob.SetCancelled(j.Cancelled)
-
-	zjob.SetArrayid(j.ArrayId)
-	zjob.SetGroupid(j.GroupId)
-	zjob.SetDelegatetm(j.Delegatetm)
-	zjob.SetLastpingtm(j.Lastpingtm)
-	zjob.SetUnansweredping(j.Unansweredping)
-	zjob.SetSendernonce(j.Sendernonce)
-	zjob.SetSendtime(j.Sendtime)
-
-	return zjob
-}
-
-func JobToCapnp(j *Job) (bytes.Buffer, *capn.Segment) {
-	seg := capn.NewBuffer(nil)
-	z := schema.NewRootZ(seg)
-
-	zjob := JobToCapnpSegment(j, seg)
-	z.SetJob(zjob)
-
-	buf := bytes.Buffer{}
-	seg.WriteTo(&buf)
-
-	return buf, seg
-}
-
 func recvZjob(nnzbus *nn.Socket, cfg *Config) (job *Job, err error) {
 
 	// harden against cross-cluster communication
@@ -1461,66 +1374,6 @@ func recvZjob(nnzbus *nn.Socket, cfg *Config) (job *Job, err error) {
 	buf := bytes.NewBuffer(plain)
 	job = CapnpToJob(buf)
 	return job, nil
-}
-
-func CapnpZjobToJob(zj schema.Zjob) *Job {
-	return &Job{
-		Id:       zj.Id(),
-		Msg:      zj.Msg(),
-		Aboutjid: zj.Aboutjid(),
-
-		Cmd:  zj.Cmd(),
-		Args: zj.Args().ToArray(),
-		Out:  zj.Out().ToArray(),
-		Env:  zj.Env().ToArray(),
-
-		Host: zj.Host(),
-		Stm:  zj.Stm(),
-		Etm:  zj.Etm(),
-
-		Elapsec: zj.Elapsec(),
-		Status:  zj.Status(),
-		Subtime: zj.Subtime(),
-
-		Pid: zj.Pid(),
-		Dir: zj.Dir(),
-
-		Submitaddr: zj.Submitaddr(),
-		Serveraddr: zj.Serveraddr(),
-		Workeraddr: zj.Workeraddr(),
-		Finishaddr: zj.Finishaddr().ToArray(),
-
-		Signature: zj.Signature(),
-		IsLocal:   zj.Islocal(),
-		Cancelled: zj.Cancelled(),
-
-		ArrayId:        zj.Arrayid(),
-		GroupId:        zj.Groupid(),
-		Delegatetm:     zj.Delegatetm(),
-		Lastpingtm:     zj.Lastpingtm(),
-		Unansweredping: zj.Unansweredping(),
-		Sendernonce:    zj.Sendernonce(),
-		Sendtime:       zj.Sendtime(),
-	}
-}
-
-func CapnpToJob(buf *bytes.Buffer) *Job {
-	capMsg, err := capn.ReadFromStream(buf, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	z := schema.ReadRootZ(capMsg)
-	d3 := z.Which()
-	if d3 != schema.Z_JOB {
-		panic(fmt.Sprintf("expected schema.Z_JOB, got %d", d3))
-	}
-
-	zj := z.Job()
-	job := CapnpZjobToJob(zj)
-
-	//VPrintf("[pid %d] recvZjob got Zjob message: %#v\n", os.Getpid(), job)
-	return job
 }
 
 func MakeTestJob() *Job {
@@ -1714,101 +1567,6 @@ func ts() string {
 func TSPrintf(format string, a ...interface{}) {
 	fmt.Printf("%s ", ts())
 	fmt.Printf(format, a...)
-}
-
-func (js *JobServ) ServerToCapnp() (bytes.Buffer, *capn.Segment) {
-	seg := capn.NewBuffer(nil)
-	z := schema.NewRootZ(seg)
-	zjs := schema.NewZgoqserver(seg)
-
-	// nextjobid
-	zjs.SetNextjobid(js.NextJobId)
-
-	var i int
-	var j *Job
-
-	// runq
-	if len(js.RunQ) > 0 {
-		runq := schema.NewZjobList(seg, len(js.RunQ))
-		plistRunq := capn.PointerList(runq)
-		i = 0
-		for _, j = range js.RunQ {
-			zjob := JobToCapnpSegment(j, seg)
-			plistRunq.Set(i, capn.Object(zjob))
-			i++
-		}
-		zjs.SetRunq(runq)
-	}
-
-	// waitingjobs
-	if len(js.WaitingJobs) > 0 {
-		waitingjobs := schema.NewZjobList(seg, len(js.WaitingJobs))
-		plistWaitingjobs := capn.PointerList(waitingjobs)
-		i = 0
-		for _, j = range js.WaitingJobs {
-			zjob := JobToCapnpSegment(j, seg)
-			plistWaitingjobs.Set(i, capn.Object(zjob))
-			i++
-		}
-		zjs.SetWaitingjobs(waitingjobs)
-	}
-	// counters
-	zjs.SetFinishedjobscount(js.FinishedJobsCount)
-	zjs.SetBadsgtcount(js.BadSgtCount)
-	zjs.SetCancelledjobcount(js.CancelledJobCount)
-	zjs.SetBadnoncecount(js.BadNonceCount)
-
-	// lastly set Z
-	z.SetGoqserver(zjs)
-
-	buf := bytes.Buffer{}
-	seg.WriteTo(&buf)
-
-	return buf, seg
-}
-
-func (js *JobServ) SetStateFromCapnp(r io.Reader, fn string) {
-	capMsg, err := capn.ReadFromStream(r, nil)
-	if err != nil {
-		panic(fmt.Errorf("capnp problem reading from file '%s': %s", fn, err))
-	}
-
-	z := schema.ReadRootZ(capMsg)
-	d3 := z.Which()
-	if d3 != schema.Z_GOQSERVER {
-		panic(fmt.Sprintf("expected schema.Z_GOQSERVER, got %d", d3))
-	}
-
-	zjs := z.Goqserver()
-	js.NextJobId = zjs.Nextjobid()
-
-	runqlist := zjs.Runq().ToArray()
-	for _, zjob := range runqlist {
-		j := CapnpZjobToJob(zjob)
-
-		// don't kill jobs prematurely just because we the server
-		// just started back up!
-		j.Unansweredping = 0
-		j.Lastpingtm = time.Now().UnixNano()
-
-		js.RunQ[j.Id] = j
-		js.KnownJobHash[j.Id] = j
-		js.RegisterWho(j)
-	}
-
-	waitlist := zjs.Waitingjobs().ToArray()
-	for _, zjob := range waitlist {
-		j := CapnpZjobToJob(zjob)
-		js.WaitingJobs = append(js.WaitingJobs, j)
-		js.KnownJobHash[j.Id] = j
-		js.RegisterWho(j)
-	}
-
-	js.FinishedJobsCount = zjs.Finishedjobscount()
-	js.BadSgtCount = zjs.Badsgtcount()
-	js.CancelledJobCount = zjs.Cancelledjobcount()
-	js.BadNonceCount = zjs.Badnoncecount()
-
 }
 
 // if we have an old state, file, load it and blow it away
---
+++ ser.go
@@ -0,0 +1,258 @@
+package main
+
+// ser.go : serialization
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"time"
+
+	schema "github.com/glycerine/goq/schema"
+	"github.com/glycerine/go-capnproto"
+)
+
+func (js *JobServ) ServerToCapnp() (bytes.Buffer, *capn.Segment) {
+	seg := capn.NewBuffer(nil)
+	z := schema.NewRootZ(seg)
+
+	zjs := schema.NewZgoqserver(seg)
+	z.SetGoqserver(zjs)
+
+	// nextjobid
+	zjs.SetNextjobid(js.NextJobId)
+
+	var i int
+	var j *Job
+
+	// runq
+	if len(js.RunQ) > 0 {
+		//fmt.Printf("len of Runq: %d, %#v\n", len(js.RunQ), js.RunQ)
+		runq := schema.NewZjobList(seg, len(js.RunQ))
+		plistRunq := capn.PointerList(runq)
+		i = 0
+		for _, j = range js.RunQ {
+			zjob := JobToCapnpSegment(j, seg, false)
+			plistRunq.Set(i, capn.Object(zjob))
+			i++
+		}
+		zjs.SetRunq(runq)
+	}
+
+	// waitingjobs
+	if len(js.WaitingJobs) > 0 {
+		//fmt.Printf("len of WaitingJobs: %d, %#v\n", len(js.WaitingJobs), js.WaitingJobs)
+		waitingjobs := schema.NewZjobList(seg, len(js.WaitingJobs))
+		plistWaitingjobs := capn.PointerList(waitingjobs)
+		i = 0
+		for _, j = range js.WaitingJobs {
+			zjob := JobToCapnpSegment(j, seg, false)
+			plistWaitingjobs.Set(i, capn.Object(zjob))
+			i++
+		}
+		zjs.SetWaitingjobs(waitingjobs)
+	}
+	// counters
+	zjs.SetFinishedjobscount(js.FinishedJobsCount)
+	zjs.SetBadsgtcount(js.BadSgtCount)
+	zjs.SetCancelledjobcount(js.CancelledJobCount)
+	zjs.SetBadnoncecount(js.BadNonceCount)
+
+	buf := bytes.Buffer{}
+	seg.WriteTo(&buf)
+
+	return buf, seg
+}
+
+func (js *JobServ) SetStateFromCapnp(r io.Reader, fn string) {
+	capMsg, err := capn.ReadFromStream(r, nil)
+	if err != nil {
+		panic(fmt.Errorf("capnp problem reading from file '%s': %s", fn, err))
+	}
+
+	z := schema.ReadRootZ(capMsg)
+	d3 := z.Which()
+	if d3 != schema.Z_GOQSERVER {
+		panic(fmt.Sprintf("expected schema.Z_GOQSERVER, got %d", d3))
+	}
+
+	zjs := z.Goqserver()
+	js.NextJobId = zjs.Nextjobid()
+
+	runqlist := zjs.Runq().ToArray()
+	for _, zjob := range runqlist {
+		j := CapnpZjobToJob(zjob)
+
+		// don't kill jobs prematurely just because we the server
+		// just started back up!
+		j.Unansweredping = 0
+		j.Lastpingtm = time.Now().UnixNano()
+
+		js.RunQ[j.Id] = j
+		js.KnownJobHash[j.Id] = j
+		js.RegisterWho(j)
+	}
+
+	waitlist := zjs.Waitingjobs().ToArray()
+	for _, zjob := range waitlist {
+		j := CapnpZjobToJob(zjob)
+		js.WaitingJobs = append(js.WaitingJobs, j)
+		js.KnownJobHash[j.Id] = j
+		js.RegisterWho(j)
+	}
+
+	js.FinishedJobsCount = zjs.Finishedjobscount()
+	js.BadSgtCount = zjs.Badsgtcount()
+	js.CancelledJobCount = zjs.Cancelledjobcount()
+	js.BadNonceCount = zjs.Badnoncecount()
+
+}
+
+func CapnpZjobToJob(zj schema.Zjob) *Job {
+
+	return &Job{
+		Id:       zj.Id(),
+		Msg:      zj.Msg(),
+		Aboutjid: zj.Aboutjid(),
+
+		Cmd:  zj.Cmd(),
+		Args: zj.Args().ToArray(),
+		Out:  zj.Out().ToArray(),
+		Env:  zj.Env().ToArray(),
+
+		Host: zj.Host(),
+		Stm:  zj.Stm(),
+		Etm:  zj.Etm(),
+
+		Elapsec: zj.Elapsec(),
+		Status:  zj.Status(),
+		Subtime: zj.Subtime(),
+
+		Pid: zj.Pid(),
+		Dir: zj.Dir(),
+
+		Submitaddr: zj.Submitaddr(),
+		Serveraddr: zj.Serveraddr(),
+		Workeraddr: zj.Workeraddr(),
+		Finishaddr: zj.Finishaddr().ToArray(),
+
+		Signature: zj.Signature(),
+		IsLocal:   zj.Islocal(),
+		Cancelled: zj.Cancelled(),
+
+		ArrayId:        zj.Arrayid(),
+		GroupId:        zj.Groupid(),
+		Delegatetm:     zj.Delegatetm(),
+		Lastpingtm:     zj.Lastpingtm(),
+		Unansweredping: zj.Unansweredping(),
+		Sendernonce:    zj.Sendernonce(),
+		Sendtime:       zj.Sendtime(),
+	}
+}
+
+func CapnpToJob(buf *bytes.Buffer) *Job {
+	capMsg, err := capn.ReadFromStream(buf, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	z := schema.ReadRootZ(capMsg)
+	d3 := z.Which()
+	if d3 != schema.Z_JOB {
+		panic(fmt.Sprintf("expected schema.Z_JOB, got %d", d3))
+	}
+
+	zj := z.Job()
+	job := CapnpZjobToJob(zj)
+
+	return job
+}
+
+// once you've already allocated a segment
+func JobToCapnpSegment(j *Job, seg *capn.Segment, shorten bool) schema.Zjob {
+
+	zjob := schema.NewZjob(seg)
+
+	zjob.SetId(j.Id)
+
+	if shorten {
+		return zjob
+	}
+
+	zjob.SetAboutjid(j.Aboutjid)
+	zjob.SetMsg(j.Msg)
+
+	zjob.SetCmd(j.Cmd)
+
+	if tl := StringSliceToCapnp(j.Out, seg); tl != nil {
+		zjob.SetOut(*tl)
+	}
+
+	if tl := StringSliceToCapnp(j.Args, seg); tl != nil {
+		zjob.SetArgs(*tl)
+	}
+
+	if tl := StringSliceToCapnp(j.Env, seg); tl != nil {
+		zjob.SetEnv(*tl)
+	}
+
+	zjob.SetHost(j.Host)
+
+	zjob.SetStm(int64(j.Stm))
+	zjob.SetEtm(int64(j.Etm))
+	zjob.SetElapsec(j.Elapsec)
+
+	zjob.SetStatus(j.Status)
+	zjob.SetSubtime(int64(j.Subtime))
+	zjob.SetPid(j.Pid)
+
+	zjob.SetDir(j.Dir)
+
+	zjob.SetSubmitaddr(j.Submitaddr)
+	zjob.SetServeraddr(j.Serveraddr)
+	zjob.SetWorkeraddr(j.Workeraddr)
+
+	if tl := StringSliceToCapnp(j.Finishaddr, seg); tl != nil {
+		zjob.SetFinishaddr(*tl)
+	}
+
+	zjob.SetSignature(j.Signature)
+	zjob.SetIslocal(j.IsLocal)
+	zjob.SetCancelled(j.Cancelled)
+
+	zjob.SetArrayid(j.ArrayId)
+	zjob.SetGroupid(j.GroupId)
+	zjob.SetDelegatetm(j.Delegatetm)
+	zjob.SetLastpingtm(j.Lastpingtm)
+	zjob.SetUnansweredping(j.Unansweredping)
+	zjob.SetSendernonce(j.Sendernonce)
+	zjob.SetSendtime(j.Sendtime)
+
+	return zjob
+}
+
+func JobToCapnp(j *Job) (bytes.Buffer, *capn.Segment) {
+	seg := capn.NewBuffer(nil)
+	z := schema.NewRootZ(seg)
+
+	zjob := JobToCapnpSegment(j, seg, false)
+	z.SetJob(zjob)
+
+	buf := bytes.Buffer{}
+	seg.WriteTo(&buf)
+
+	return buf, seg
+}
+
+func StringSliceToCapnp(a []string, seg *capn.Segment) *capn.TextList {
+	if a == nil {
+		panic("string slice can't be nil")
+	}
+	// looks like we need a size zero textlist for complete initialization,
+	// so do this even if len(a) == 0
+	tl := seg.NewTextList(len(a))
+	for i := range a {
+		tl.Set(i, a[i])
+	}
+	return &tl
+}
