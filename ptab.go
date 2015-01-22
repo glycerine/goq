7b8892cd64ce81d6a247a00fd133c03fcf295115
Jason E. Aten <j.e.aten@gmail.com>
2015-01-21 23:32:28 -0800

    latest ptab

--- ptab.go
+++ ptab.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os/exec"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // process table generation
@@ -93,4 +95,110 @@ func LinuxPs() *map[int]bool {
 		}
 	}
 	return &res
+}
+
+func OpenFiles(pid int) []string {
+	switch Sysname {
+	case "Linux":
+		return LinuxOpenFiles(pid)
+
+	case "Darwin":
+		return DarwinOpenFiles(pid)
+	}
+	return []string{}
+}
+
+func LinuxOpenFiles(jobservPid int) []string {
+
+	// wait until process shows up (in /proc or ps)
+	waited := 0
+	for {
+		pt := *ProcessTable()
+		_, jsAlive := pt[jobservPid]
+		if jsAlive {
+			break
+		}
+		time.Sleep(50 * time.Millisecond)
+		waited++
+		if waited > 10 {
+			panic(fmt.Sprintf("jobserv with expected pid %d did not show up in /proc after 10 waits", jobservPid))
+		}
+	}
+	fmt.Printf("\njobserv with expected pid %d was *found* in /proc after %d waits of 50msec\n", jobservPid, waited)
+
+	// read all the open files
+	fileInfoSlice, err := ioutil.ReadDir(fmt.Sprintf("/proc/%d/fd", jobservPid))
+	if err != nil {
+		panic(err)
+	}
+
+	res := make([]string, len(fileInfoSlice))
+	for i := range fileInfoSlice {
+		res[i] = fileInfoSlice[i].Name()
+	}
+
+	return res
+}
+
+func DarwinOpenFiles(pid int) []string {
+	// wait until process shows up (in /proc or ps)
+	waited := 0
+	for {
+		pt := *ProcessTable()
+		_, jsAlive := pt[pid]
+		if jsAlive {
+			break
+		}
+		time.Sleep(50 * time.Millisecond)
+		waited++
+		if waited > 10 {
+			panic(fmt.Sprintf("jobserv with expected pid %d did not show up in /proc after 10 waits", pid))
+		}
+	}
+	fmt.Printf("\njobserv with expected pid %d was *found* in /proc after %d waits of 50msec\n", pid, waited)
+
+	spid := fmt.Sprintf("%d", pid)
+	//fmt.Printf("about to do: lsof -p %s -Fnt\n", spid)
+	o, err := exec.Command("/usr/sbin/lsof", "-p", spid, "-Fnt").Output()
+	if err != nil {
+		panic(err)
+	}
+	s := strings.Split(string(o), "\n")
+
+	// join the name and type together on one line
+	// skip the header line
+	s = s[1:]
+	r := make([]string, 0)
+	n := len(s) - 1 // empty string at end
+	//fmt.Printf("len(s) = %d; s='%#v'\n", len(s), s)
+	for i := 0; i < n; i += 2 {
+		//fmt.Printf("on i = %d, s[i]='%s', s[i+i]='%s'\n", i, s[i], s[i+1])
+		r = append(r, s[i]+":"+s[i+1])
+	}
+
+	//fmt.Printf("DarwinOpenFiles got back from lsof:\n")
+	//ShowStrings(r)
+
+	return r
+}
+
+func SetDiff(a, b []string) []string {
+	res := make([]string, 0)
+	m := make(map[string]bool)
+	for i := range a {
+		m[a[i]] = true
+	}
+	for i := range b {
+		delete(m, b[i])
+	}
+	for s := range m {
+		res = append(res, s)
+	}
+	return res
+}
+
+func ShowStrings(s []string) {
+	for _, line := range s {
+		fmt.Printf("%s\n", line)
+	}
 }
